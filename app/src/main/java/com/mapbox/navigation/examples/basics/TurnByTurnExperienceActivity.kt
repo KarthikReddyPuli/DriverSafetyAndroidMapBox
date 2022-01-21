cc56901d26f65eb064197a74cef382ccb44edb7d
tnfssc <tnfssc@gmail.com>
2022-01-21 14:41:39 +0530

    Modules upgraded and Nav working

--- app/src/main/java/com/mapbox/navigation/examples/basics/TurnByTurnExperienceActivity.kt
+++ app/src/main/java/com/mapbox/navigation/examples/basics/TurnByTurnExperienceActivity.kt
@@ -33,10 +33,6 @@ import com.mapbox.navigation.core.MapboxNavigation
 import com.mapbox.navigation.core.MapboxNavigationProvider
 import com.mapbox.navigation.core.directions.session.RoutesObserver
 import com.mapbox.navigation.core.formatter.MapboxDistanceFormatter
-import com.mapbox.navigation.core.replay.MapboxReplayer
-import com.mapbox.navigation.core.replay.ReplayLocationEngine
-import com.mapbox.navigation.core.replay.route.ReplayProgressObserver
-import com.mapbox.navigation.core.replay.route.ReplayRouteMapper
 import com.mapbox.navigation.core.trip.session.LocationMatcherResult
 import com.mapbox.navigation.core.trip.session.LocationObserver
 import com.mapbox.navigation.core.trip.session.RouteProgressObserver
@@ -93,8 +89,6 @@ import java.util.Locale
  *
  * How to use this example:
  * - You can long-click the map to select a destination.
- * - The guidance will start to the selected destination while simulating location updates.
- * You can disable simulation by commenting out the [replayLocationEngine] setter in [NavigationOptions].
  * Then, the device's real location will be used.
  * - At any point in time you can finish guidance or select a new destination.
  * - You can use buttons to mute/unmute voice instructions, recenter the camera, or show the route overview.
@@ -104,21 +98,6 @@ class TurnByTurnExperienceActivity : AppCompatActivity() {
     private companion object {
         private const val BUTTON_ANIMATION_DURATION = 1500L
     }
-
-    /**
-     * Debug tool used to play, pause and seek route progress events that can be used to produce mocked location updates along the route.
-     */
-    private val mapboxReplayer = MapboxReplayer()
-
-    /**
-     * Debug tool that mocks location updates with an input from the [mapboxReplayer].
-     */
-    private val replayLocationEngine = ReplayLocationEngine(mapboxReplayer)
-
-    /**
-     * Debug observer that makes sure the replayer has always an up-to-date information to generate mock updates.
-     */
-    private val replayProgressObserver = ReplayProgressObserver(mapboxReplayer)
 
     /**
      * Bindings to the example layout.
@@ -434,8 +413,6 @@ class TurnByTurnExperienceActivity : AppCompatActivity() {
             MapboxNavigationProvider.create(
                 NavigationOptions.Builder(this.applicationContext)
                     .accessToken(getString(R.string.mapbox_access_token))
-                    // comment out the location engine setting block to disable simulation
-                    .locationEngine(replayLocationEngine)
                     .build()
             )
         }
@@ -528,7 +505,7 @@ class TurnByTurnExperienceActivity : AppCompatActivity() {
 
         // load map style
         mapboxMap.loadStyleUri(
-            Style.MAPBOX_STREETS
+            Style.DARK
         ) {
             // add long click listener that search for a route to the clicked destination
             binding.mapView.gestures.addOnMapLongClickListener { point ->
@@ -570,22 +547,6 @@ class TurnByTurnExperienceActivity : AppCompatActivity() {
         mapboxNavigation.registerRouteProgressObserver(routeProgressObserver)
         mapboxNavigation.registerLocationObserver(locationObserver)
         mapboxNavigation.registerVoiceInstructionsObserver(voiceInstructionsObserver)
-        mapboxNavigation.registerRouteProgressObserver(replayProgressObserver)
-
-        if (mapboxNavigation.getRoutes().isEmpty()) {
-            // if simulation is enabled (ReplayLocationEngine set to NavigationOptions)
-            // but we're not simulating yet,
-            // push a single location sample to establish origin
-            mapboxReplayer.pushEvents(
-                listOf(
-                    ReplayRouteMapper.mapToUpdateLocation(
-                        eventTimestamp = 0.0,
-                        point = Point.fromLngLat(-122.39726512303575, 37.785128345296805)
-                    )
-                )
-            )
-            mapboxReplayer.playFirstLocation()
-        }
     }
 
     override fun onStop() {
@@ -596,13 +557,11 @@ class TurnByTurnExperienceActivity : AppCompatActivity() {
         mapboxNavigation.unregisterRouteProgressObserver(routeProgressObserver)
         mapboxNavigation.unregisterLocationObserver(locationObserver)
         mapboxNavigation.unregisterVoiceInstructionsObserver(voiceInstructionsObserver)
-        mapboxNavigation.unregisterRouteProgressObserver(replayProgressObserver)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         MapboxNavigationProvider.destroy()
-        mapboxReplayer.finish()
         maneuverApi.cancel()
         routeLineApi.cancel()
         routeLineView.cancel()
@@ -666,9 +625,6 @@ class TurnByTurnExperienceActivity : AppCompatActivity() {
         // will be used for active guidance
         mapboxNavigation.setRoutes(routes)
 
-        // start location simulation along the primary route
-        startSimulation(routes.first())
-
         // show UI elements
         binding.soundButton.visibility = View.VISIBLE
         binding.routeOverview.visibility = View.VISIBLE
@@ -682,24 +638,10 @@ class TurnByTurnExperienceActivity : AppCompatActivity() {
         // clear
         mapboxNavigation.setRoutes(listOf())
 
-        // stop simulation
-        mapboxReplayer.stop()
-
         // hide UI elements
         binding.soundButton.visibility = View.INVISIBLE
         binding.maneuverView.visibility = View.INVISIBLE
         binding.routeOverview.visibility = View.INVISIBLE
         binding.tripProgressCard.visibility = View.INVISIBLE
-    }
-
-    private fun startSimulation(route: DirectionsRoute) {
-        mapboxReplayer.run {
-            stop()
-            clearEvents()
-            val replayEvents = ReplayRouteMapper().mapDirectionsRouteGeometry(route)
-            pushEvents(replayEvents)
-            seekTo(replayEvents.first())
-            play()
-        }
     }
 }
